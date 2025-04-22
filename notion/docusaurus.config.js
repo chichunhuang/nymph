49b0acc151ba6fcc933f653518ce4c92a6916887
chichun <chichunhuamg@gmail.com>
2025-04-22 15:31:13 +0800

    nympth: GitPage test run, baseUrl: setting

--- notion/docusaurus.config.js
+++ notion/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/notion/', // gitpages: set same as projectName 
+  baseUrl: '/nymph/', // gitpages: set same as projectName 
   //baseUrl: '/',
 
   // GitHub pages deployment config.
